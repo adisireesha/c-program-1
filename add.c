2f22e00066eda74fe02bbd9fc46a85d8a36e6fc9
Sireesha <sireesha>
2023-02-22 15:02:07 +0530

    new_commmit

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main
+{
+        int a,b,c;
+        a=10;
+        b=5;
+        c=a+b;
+        printf("adition of two numbers are %d",c);
+}
+~
+~
+
---
+++ shiva.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(void)
+{
+int c=10,i;
+for(i=0;i<=c;i++)
+{
+printf("gytechs\n");
+}
+return 0;
+}
